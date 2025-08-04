b7ec9a8235bad51671e89ad9c7a29daae33af30b
Mohan <nyamanahallimanjun.m@northeastern.edu>
2025-08-03 22:20:43 -0700

    grpc

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,15 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
+@GrpcService
+public class BillingGrpcService  extends BillingServiceGrpc.BillingServiceImplBase {
+
+@Override
+    public void createBillingAccount(BillingRequest billingRequest){
+    Strea
+}
+
+
+}
